82e4576cbe8a5ee01a878221473f092d88e653c3
Tako Schotanus <tako@ceylon-lang.org>
2018-11-06 12:45:02 +0100

    refactor(catalog): Capabilities and Generators are now classes
    They also now have a base implementation that should make the
    basic actions that many of them have to perform easier to
    implement.

--- catalog/generators/database-secret/index.ts
+++ catalog/generators/database-secret/index.ts
@@ -1,33 +1,37 @@
 
-export const id = 'database-secret';
+import { BaseGenerator } from 'core/catalog';
 
-export async function apply(applyGenerator, resources, targetDir, props: any = {}) {
-    // Create Secret holding Database connection/authentication information
-    if (!resources.secret(props.secretName)) {
-        const secret = {
-            'kind': 'Secret',
-            'apiVersion': 'v1',
-            'metadata': {
-                'name': props.secretName,
-                'labels': {
-                    'app': props.application,
-                }
-            },
-            'stringData': {
-                'uri': props.databaseUri,
-                'database': props.databaseName,
-                'user': 'dbuser',
-                'password': 'secret',  // TODO generate pwd
-            }
-        };
-        resources.add(secret);
-        // console.log(`Secret ${props.secretName} added`);
-    } else {
-        // console.log(`Secret ${props.secretName} already exists`);
+export default class DatabaseSecret extends BaseGenerator {
+    public static readonly sourceDir: string = __dirname;
+
+    public static info() {
+        return require('./info.json');
     }
-    return resources;
-}
 
-export function info() {
-    return require('./info.json');
+    public async apply(resources, props: any = {}) {
+        // Create Secret holding Database connection/authentication information
+        if (!resources.secret(props.secretName)) {
+            const secret = {
+                'kind': 'Secret',
+                'apiVersion': 'v1',
+                'metadata': {
+                    'name': props.secretName,
+                    'labels': {
+                        'app': props.application,
+                    }
+                },
+                'stringData': {
+                    'uri': props.databaseUri,
+                    'database': props.databaseName,
+                    'user': 'dbuser',
+                    'password': 'secret',  // TODO generate pwd
+                }
+            };
+            resources.add(secret);
+            // console.log(`Secret ${props.secretName} added`);
+        } else {
+            // console.log(`Secret ${props.secretName} already exists`);
+        }
+        return resources;
+    }
 }
